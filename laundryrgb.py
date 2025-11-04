49bdef719d7a16d730f8d484cc4e1ac2f0045e07
Jayden Tran <klik@MacBook-Air-5.local>
2025-11-04 15:30:32 -0700

    Add laundryrgb.py

---
+++ laundryrgb.py
@@ -0,0 +1,32 @@
+import time
+import board
+import busio
+import adafruit_tcs34725
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_tcs34725.TCS34725(i2c)
+sensor.integration_time = 100
+sensor.gain = 60
+
+while True: 
+    r, g, b, c = sensor.color_raw
+    if c == 0:
+        continue
+    r_norm = r / c
+    g_norm = g / c
+    b_norm = b / c
+
+    rgb = (int(r_norm * 255), int(g_norm * 255), int(b_norm * 255))
+
+    print("R:", r, "G:", g, "B:", b, "C:", c,)
+    print("Color Temperature:", sensor.color_temperature, "K")
+    print("Lux:", sensor.lux)
+    print("Normalized RGB:", rgb)
+
+    if sensor.lux > 100:
+        print("Light / White Clothing Detected")
+    else:
+        print("Dark / Colored Clothing Detected")
+    print("-----------------------------")
+    time.sleep(1)
+    
