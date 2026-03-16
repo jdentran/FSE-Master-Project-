75fff200212484df65eb4d87d4865c620a60ede6
Jayden Tran <klik@MacBook-Air-6.local>
2026-03-16 02:50:44 -0700

    RGB clothing detection system
    - Implemented laundryrgb.py for RGB-based clothing detection
    - Added color detection logic
    - Improved threshold tuning for color recognition
    - Initial testing and updates

--- laundryrgb.py
+++ laundryrgb.py
@@ -5,7 +5,7 @@ import adafruit_tcs34725
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_tcs34725.TCS34725(i2c)
-sensor.integration_time = 100
+sensor.integration_time = 400
 sensor.gain = 60
 
 while True: 
@@ -16,6 +16,8 @@ while True:
     g_norm = g / c
     b_norm = b / c
 
+    brightness = 0.299 * r_norm + 0.587 * g_norm + 0.114 * b_norm
+
     rgb = (int(r_norm * 255), int(g_norm * 255), int(b_norm * 255))
 
     print("R:", r, "G:", g, "B:", b, "C:", c,)
@@ -23,7 +25,7 @@ while True:
     print("Lux:", sensor.lux)
     print("Normalized RGB:", rgb)
 
-    if sensor.lux > 100:
+    if c > 40 and brightness > 0.40:
         print("Light / White Clothing Detected")
     else:
         print("Dark / Colored Clothing Detected")
